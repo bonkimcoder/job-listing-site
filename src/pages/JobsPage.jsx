88f2fc34c39e5d28d08b57790526f74a745f396d
Boniface Kim <bonkim.wambo@gmail.com>
2024-08-08 01:22:12 +0300

    initial commit

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,11 @@
+import JobListings from "../components/JobListings";
+
+const JobsPage = () => {
+  return (
+    <section className="bg-blue-50 px-5 py-4">
+      <JobListings />
+    </section>
+  );
+};
+
+export default JobsPage;
